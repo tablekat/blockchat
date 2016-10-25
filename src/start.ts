512a53a7e154126134bcb0c7e2eb7cd7c98d5d27
katzen <katherine.hill.c9@gmail.com>
2016-10-25 02:06:44 -0500

    Changed port

--- src/start.ts
+++ src/start.ts
@@ -35,6 +35,6 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3078, function(){
+  console.log('listening on *:3078');
 });
